e0c0ab08cb4e33e4321d970edcf16cc17da2c853
DaMoTh2076 <146932113+DaMoTh2076@users.noreply.github.com>
2023-10-16 22:01:49 +0200

    JS Anpassung zum neuen html Grundgerüst - diesmal endlich final

--- MeinProjekt.js
+++ MeinProjekt.js
@@ -1,3 +1,16 @@
-// JavaScript Grundgeruest
 document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll("h1, p, a");
+  
+    elements.forEach(function(element) {
+      // Beispiel: Ändere die Schriftfarbe auf Rot
+      element.style.color = "red";
+      // Beispiel: Ändere die Schriftgröße auf 20 Pixel
+      element.style.fontSize = "20px";
+      // Beispiel: Füge einen Hintergrundfarbe hinzu
+      element.style.backgroundColor = "lightblue";
+      // Beispiel: Setze den Text in Fettschrift
+      element.style.fontWeight = "bold";
+      // Beispiel: Ändere den Abstand zwischen den Elementen
+      element.style.marginBottom = "10px";
     });
+  });
